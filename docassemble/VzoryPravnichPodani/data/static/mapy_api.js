583f6ab2547d68c1a593c3919e6f352bbafab663
Michal Kuk <michal.kuk@frankbold.org>
2020-10-23 10:44:11 +0200

    asdf

--- docassemble/VzoryPravnichPodani/data/static/mapy_api.js
+++ docassemble/VzoryPravnichPodani/data/static/mapy_api.js
@@ -15,22 +15,7 @@ suggest.addListener("suggest", function(suggestData) {
   console.log("suggest byl zavren/skryt");
 });
 
-var inputEl2 = document.querySelector("input[placeholder='Sídlo']");
-var suggest2 = new SMap.Suggest(inputEl2);
-suggest2.urlParams({
-	// omezeni pro celou CR
-	bounds: "48.5370786,12.0921668|51.0746358,18.8927040",
-  enableCategories: 1,
-  category: "street_cz,municipality_cz,address_cz",
-  type: "street|municipality|address"
-});
 
-suggest2.addListener("suggest", function(suggestData) {
-  // vyber polozky z naseptavace
-  new SMap.Geocoder(suggestData.phrase, odpoved);
-}).addListener("close", function() {
-  console.log("suggest byl zavren/skryt");
-});
 
 function odpoved(geocoder) { /* Odpověď */
     if (!geocoder.getResults()[0].results.length) {
@@ -48,6 +33,6 @@ function odpoved(geocoder) { /* Odpověď */
 function odpoved2(geocoder) {
   var results = geocoder.getResults();
   var adresa = results.label.split(", ");
-  setField("x.address", adresa[0])
+  setField("x.address", "Řekni Rumburk, Srabe")
   console.log(results)
 }
