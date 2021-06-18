a8d3e82afdbd92de40b6b6127a7ae3d200218a8a
Michal Kuk <servis@frankbold.org>
2021-06-18 13:33:50 +0200

    Opravy nečinnosti a drobná doplnění

---
+++ docassemble/VzoryPravnichPodani/progressivedisclosure.py
@@ -0,0 +1,14 @@
+import re
+
+__all__ = ['prog_disclose']
+
+def prog_disclose(template, classname=None):
+    if classname is None:
+        classname = ' bg-light'
+    else:
+        classname = ' ' + classname.strip()
+    the_id = re.sub(r'[^A-Za-z0-9]', '', template.instanceName)
+    return u"""\
+<a class="collapsed" data-toggle="collapse" href="#{}" role="button" aria-expanded="false" aria-controls="collapseExample"><span class="pdcaretopen"><i class="fas fa-caret-down"></i></span><span class="pdcaretclosed"><i class="fas fa-caret-right"></i></span> {}</a>
+<div class="collapse" id="{}"><div class="card card-body{} pb-1">{}</div></div>\
+""".format(the_id, template.subject_as_html(trim=True), the_id, classname, template.content_as_html())
