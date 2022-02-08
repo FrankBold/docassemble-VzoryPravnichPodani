dd620f2cffd9f5c6aeee7967cd33032ec8a4a873
Michal Kuk <servis@frankbold.org>
2022-02-08 15:15:34 +0100

    Doplnění vzoru o osvobození od poplatku za odpad

---
+++ docassemble/VzoryPravnichPodani/data/static/mapy_api_obec.js
@@ -0,0 +1,29 @@
+var inputEl = document.querySelector("input[type='text']");
+var suggest = new SMap.Suggest(inputEl, {
+	provider: new SMap.SuggestProvider({
+  	updateParams: params => {
+      params.enableCategories = 1;
+      params.category = "municipality_cz";
+      params.type = "municipality";
+      params.lang = "cs";
+      params.locality = "cz";
+    }
+  }),
+    delay: 50,
+});
+suggest.addListener("suggest", suggestData => {
+  // vyber polozky z naseptavace
+  setTimeout(function() {
+    var coords = SMap.Coords.fromWGS84(suggestData.data.longitude, suggestData.data.latitude);
+    new SMap.Geocoder.Reverse(coords, odpoved);
+  }, 0);
+});
+
+var odpoved = function(geocoder) {
+    var results = geocoder.getResults();
+      for (let  i  = 0; i < results.items.length; i++) {
+        if (results.items[i].type == "regi") {
+          setField("Obec." + typ + ".kraj", results.items[i].name)
+        }
+  }
+}
---
+++ docassemble/VzoryPravnichPodani/poplatekOdpad.py
@@ -0,0 +1,76 @@
+import csv
+import re
+import json
+import requests
+import io
+import docx
+import textract
+import tempfile
+from docassemble.base.util import path_and_mimetype, ocr_file, DAObject, DAFile
+
+(filename, mimetype) = path_and_mimetype('data/static/obce.json')
+
+def najitObec(obec, kraj):
+    with open(filename, encoding='utf-8') as d:
+        data = json.load(d)
+    for polozka in data["municipalities"]:
+        if obec == polozka["hezkyNazev"] and re.sub('(k|K)raj', '', kraj).strip() == re.sub('(k|K)raj', '', polozka["adresaUradu"]["kraj"]).strip():
+            return(polozka)
+    return(False)
+
+def vyhlaskyObce(idds):
+    with requests.Session() as s:
+        r = s.get('https://sbirkapp.gov.cz/vyhledavani/vysledek?typ=ozv&ovm='+idds+'&kod_vusc=&ucinnost_od=&ucinnost_do=&oblast=&zmocneni=&nazev=&znacka=&format_exportu=csv')
+        r = r.content.decode('utf-8')
+
+        r = csv.DictReader(r.splitlines())
+
+        vyhlasky = []
+
+        for radek in r:
+            vyhlasky.append(radek)
+
+        return(vyhlasky)
+
+def vyhlaskaText(id):
+    r = requests.get('https://sbirkapp.gov.cz/detail/'+id+'/text')
+    if "pdf" in r.headers['content-type']:
+      thefile = DAFile("thefile")
+      thefile.initialize(filename="vyhlaska.pdf", extension="pdf")
+      thefile.write(r.content, binary="true")
+      return(ocr_file(thefile, language="cs"))
+    elif "docx" in r.headers['content-type']:
+      obsah = []
+      doc = docx.Document(io.BytesIO(r.content))
+      for para in doc.paragraphs:
+        obsah.append(para.text)
+      return('\n'.join(obsah))
+    elif "doc" == r.headers['content-type'] or "application/msword" == r.headers['content-type']:
+      r = requests.post('https://hook.integromat.com/7nmnpym8m6g0byshhd2d7e6jh6y8gcjn?url=https://sbirkapp.gov.cz/detail/'+id+'/text')
+      text = r.content.decode('utf-8')
+      return(text)
+    elif "odt" in r.headers['content-type']:
+      try:
+        with tempfile.NamedTemporaryFile() as tmp:
+          r = requests.get('https://sbirkapp.gov.cz/detail/'+id+'/text')
+          tmp.write(r.content)
+          text = textract.process(tmp.name, extension='odt').decode('utf-8')
+          return(text)
+      except Exception as e:
+          return(e)
+    else:
+      return(r.headers['content-type'])
+
+def typPoplatku(text):
+    text = text[:800]
+    if "místním poplatku" not in text:
+        return [False,"Vyhláška se zřejmě nezabývá poplatkem za odpad."]
+    else:
+        if "za odkládání komunálního odpadu" in text and "obecní systém odpadového hospodářství" in text:
+            return [False,"Obojí..."]
+        elif "za odkládání komunálního odpadu" in text:
+            return [True,"znemovitosti"]
+        elif "systém odpadového hospodářství" in text:
+            return [True,"system"]
+        else:
+            return [False,"Ani jedno"]
